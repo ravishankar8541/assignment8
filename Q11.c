a65c78a9602ca0556a2feb3e9a1fb871acc9392f
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-07-23 15:00:32 +0530

    Add files via upload

---
+++ Q11.c
@@ -0,0 +1,26 @@
+/*
+             A
+          A  B  A
+       A  B  C  B  A
+    A  B  C  D  C  B  A
+ A  B  C  D  E  D  C  B  A     
+ */
+#include"stdio.h"
+int main()
+{
+    int i,j;
+    int k;
+    for(i=1;i<=5;i++){
+        k='A';
+        for(j=1;j<=9;j++){
+            if(j>=6-i && j<=4+i){
+                printf("%c ",k);
+                j<5?k++:k--;
+            }else
+            printf("  ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
